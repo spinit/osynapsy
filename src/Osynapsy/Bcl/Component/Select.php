62df4c734fc4e155efc806422318c550ce30640c
pietr <pietr@MARTE.fritz.box>
2016-07-26 16:16:49 +0200

    Fix multiselect

--- src/Osynapsy/Bcl/Component/Select.php
+++ src/Osynapsy/Bcl/Component/Select.php
@@ -11,19 +11,25 @@ use Osynapsy\Ocl\Component\ComboBox2;
 class Select extends ComboBox2
 {
     //put your code here
-    public function __construct($name)
-    {
+    public function __construct($name, $multiple=false)
+    {        
         parent::__construct($name);
         $this->class = 'osy-select';
         $this->requireCss('/vendor/osynapsy/Bcl/Select/bootstrap-select.css');
         $this->requireJs('/vendor/osynapsy/Bcl/Select/bootstrap-select.js');
         $this->requireJs('/vendor/osynapsy/Bcl/Select/script.js');
-        $this->par('option-select-disable',true);
+        //$this->par('option-select-disable',false);
+        if ($multiple) {
+            $this->setMultiSelect();
+        }
     }
     
     public function setMultiSelect()
     {
         $this->att('multiple','multiple');
+        if (strpos($this->name,'[') === false) {
+            $this->name = $this->name.'[]';
+        }
         return $this;
     }
 }
