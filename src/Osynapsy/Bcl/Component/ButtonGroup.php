3c2aed3b6fe3550b3d11ef34c5bd7713d4db37d5
pietr <pietr@MARTE.fritz.box>
2016-07-28 07:37:57 +0200

    Add ButtonGroup component class

---
+++ src/Osynapsy/Bcl/Component/ButtonGroup.php
@@ -0,0 +1,51 @@
+<?php
+namespace Osynapsy\Bcl\Component;
+
+use Osynapsy\Ocl\Component\Component;
+use Osynapsy\Core\Lib\Tag;
+/**
+ * Impelementation of Bootstrap ButtonGroup
+ *
+ * @author Pietro Celeste
+ */
+class ButtonGroup extends Component
+{
+    protected $ul;
+    protected $b1;
+    protected $b2;
+    
+    public function __construct($name, $label, $class='')
+    {
+        parent::__construct('div', $name);
+        $this->att('class','button-group');
+        
+        //Label button
+        $this->b1 = $this->add(new Button('btn1'.$name))
+                         ->att('class',"btn $class", true);
+        $this->b1->add($label);
+        
+        //Dropdown button
+        $this->b2 = $this->add(new Button('btn2'.$name))
+                         ->att('class',"btn dropdown-toggle $class", true)
+                         ->att('data-toggle','dropdown')
+                         ->att('aria-haspopup','true')
+                         ->att('aria-expandend','false');
+        $this->b2->add('<span class="caret"></span>');
+        $this->b2->add('<span class="sr-only">Toggle Dropdown</span>');
+        
+        //Menu container
+        $this->ul = $this->add(new Tag('ul'))->att('class','dropdown-menu');
+    }
+    
+    public function push($item)
+    {        
+        $li = $this->ul->add(new Tag('li'));
+        $li->add($item);        
+        return is_string($item) ? $this : $item;       
+    }
+    
+    public function addSeparator()
+    {
+        $this->ul(new Tag('li'))->att('class','divider')->att('role','separator');
+    }
+}
