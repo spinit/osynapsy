d17b1cb74e033c99f598571626125b51c3f75a37
pietr <pietr@MARTE.fritz.box>
2016-09-09 14:31:40 +0200

    Adjust Dropdown component

--- src/Osynapsy/Bcl/Component/Dropdown.php
+++ src/Osynapsy/Bcl/Component/Dropdown.php
@@ -10,21 +10,38 @@ class Dropdown extends Component
 {
     private $list;
     
-    public function __construct($name, $label)
+    public function __construct($name, $label, $tag='div')
     {
-        parent::__construct('div');
+        parent::__construct($tag);
         $this->add(new HiddenBox($name));
         $this->att('class','dropdown')
              ->add(new Button($name.'_btn'))
+             ->att('type', 'button')
              ->att('class','dropdown-toggle',true)
              ->att('data-toggle','dropdown')
+             ->att('aria-haspopup','true')
+             ->att('aria-expanded','false')
              ->add($label.' <span class="caret"></span>');
-        $this->list = $this->add(new ListUnordered($name.'-list','ul'));
-        $this->list->att('class','dropdown-menu')->att('aria-labelledby',$this->id);
+        $this->list = $this->add(
+            new Tag('ul')
+        )->att('class','dropdown-menu')
+         ->att('aria-labelledby',$name);
+
+    }
+    
+    protected function __build_extra__()
+    {
+        foreach ($this->data as $rec) {
+            $rec = array_values($rec);
+            $this->list
+                 ->add(new Tag('li'))
+                 ->att('data-value',$rec[0])
+                 ->add('<a href="#">'.$rec[1].'</a>');
+        }
     }
     
     public function setData($data)
     {
-        $this->list->setData($data);
+        $this->data = $data;
     }
 }
