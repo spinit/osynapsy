a786b4b369f4ccd4817f1443177f55fb96459c46
pietr <pietr@MARTE.fritz.box>
2016-12-13 09:13:18 +0100

    + Change getResponse method to run + Add getResponse method (return response) + Add setResponse method + Change keyCommand property to actionKey

--- src/Osynapsy/Core/Controller/Controller.php
+++ src/Osynapsy/Core/Controller/Controller.php
@@ -2,22 +2,24 @@
 namespace Osynapsy\Core\Controller;
 
 use Osynapsy\Core\Request\Request;
+
 use Osynapsy\Ocl\Response\Html as HtmlResponse;
+use Osynapsy\Core\Response\Response;
 use Osynapsy\Core\Response\JsonResponse;
 use Osynapsy\Core\Kernel;
 
 abstract class Controller implements InterfaceController
 {
+    protected $actionKey = 'k-cmd';
+    protected $db;
     private $parameters;
     private $templateId;
-    private $view;
-    protected $keyCommand = 'k-cmd';
-    protected $db;
     public $model;
     public $request;
     public $response;
     public $app;
-    
+    private $view;
+        
     public function __construct(Request $request = null, $db = null, $appController = null)
     {
         $this->templateId = $request->get('page.templateId');
@@ -28,10 +30,6 @@ abstract class Controller implements InterfaceController
         $this->init();
     }
     
-    abstract public function indexAction();
-    
-    abstract public function init();
-
     public function deleteAction()
     {
         if ($this->model) {
@@ -39,9 +37,9 @@ abstract class Controller implements InterfaceController
         }
     }
     
-    private function execCommand()
+    private function execAction()
     {
-        $this->response = new JsonResponse();
+        $this->setResponse(new JsonResponse());
         $cmd = $_REQUEST[$this->keyCommand];
         //sleep(0.7);
         if (!method_exists($this, $cmd.'Action')) {
@@ -62,12 +60,36 @@ abstract class Controller implements InterfaceController
         return $this->db;
     }
     
+    public function getParameter($key)
+    {
+        return (is_array($this->parameters) && array_key_exists($key,$this->parameters)) ? $this->parameters[$key] : null;
+    }
+    
     public function getResponse()
     {
-        if (!empty($_REQUEST[$this->keyCommand])) {
-            return $this->execCommand();
+        return $this->response;
+    }
+    
+    abstract public function indexAction();
+    
+    abstract public function init();
+    
+    public function loadView($path, $params = array(), $return = false)
+    {
+        $params = array('Db' => $this->db, 'controller' => $this);
+        $view = $this->response->getBuffer($path, $params);
+        if ($return) {
+            return $view;
+        }
+        $this->response->addContent($view);
+    }
+    
+    public function run()
+    {
+        if (!empty($_REQUEST[$this->actionKey])) {
+            return $this->execAction();
         }        
-        $this->response = new HtmlResponse();        
+        $this->setResponse(new HtmlResponse());
         if ($path = Kernel::get('layouts.'.$this->templateId)) {
             $this->response->template = $this->response->getBuffer($path, $this);            
         }
@@ -81,21 +103,6 @@ abstract class Controller implements InterfaceController
         return $this->response;
     }
     
-    public function getParameter($key)
-    {
-        return (is_array($this->parameters) && array_key_exists($key,$this->parameters)) ? $this->parameters[$key] : null;
-    }
-    
-    public function loadView($path, $params = array(), $return = false)
-    {
-        $params = array('Db' => $this->db, 'controller' => $this);
-        $view = $this->response->getBuffer($path, $params);
-        if ($return) {
-            return $view;
-        }
-        $this->response->addContent($view);
-    }
-    
     public function saveAction()
     {
         if ($this->model) {
@@ -106,5 +113,10 @@ abstract class Controller implements InterfaceController
     public function setDbHandler($db)
     {
         $this->db = $db;
+    }
+    
+    public function setResponse(Response $response)
+    {
+        $this->response = $response;
     }
 }
