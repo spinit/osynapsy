e0c34a4e903cd48aef4cf84e6bf9d5bbf2d04399
Pietro Celeste <p.celeste@osynapsy.org>
2016-12-27 10:55:27 +0100

    Fix assets path for static files

--- src/Osynapsy/Core/Kernel.php
+++ src/Osynapsy/Core/Kernel.php
@@ -29,7 +29,7 @@ class Kernel
         self::$request = new Request($_GET, $_POST, array(), $_COOKIE, $_FILES, $_SERVER);
         self::$router = new Router($requestRoute, self::$request);
         self::$router->loadXml(self::$repo['xmlconfig'], '/configuration/routes/route');       
-        self::$router->addRoute('OsynapsyAssetsManager','/__asset/osynapsy/?*','Osynapsy\\Core\\Helper\\AssetLoader','','Osynapsy');
+        self::$router->addRoute('OsynapsyAssetsManager','/__assets/osynapsy/?*','Osynapsy\\Core\\Helper\\AssetLoader','','Osynapsy');
         if (self::runAppController()) {
             $response = self::runRouteController(self::$router->getRoute('controller'));
             if ($response !== false) {
