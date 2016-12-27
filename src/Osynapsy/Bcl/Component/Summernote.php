266bb2d37989739a9e26ec1638a0fc12c8addf73
Pietro Celeste <p.celeste@osynapsy.org>
2016-12-27 08:45:48 +0100

    Adjusted path of asset file

--- src/Osynapsy/Bcl/Component/Summernote.php
+++ src/Osynapsy/Bcl/Component/Summernote.php
@@ -1,16 +1,14 @@
 <?php
 namespace Osynapsy\Bcl\Component;
 
-use Osynapsy\Core\Kernel;
-
 class Summernote extends TextArea
 {
     public function __construct($name)
     {
         parent::__construct($name);        
         $this->att('class','summernote');
-        Kernel::$controller->response->addContent('<link rel="stylesheet" href="/vendor/bootstrap_template/vendor/summernote/dist/summernote.css">','head');
-        Kernel::$controller->response->addContent('<script src="/vendor/bootstrap_template/vendor/summernote/dist/summernote.js"></script>','head');
-        Kernel::$controller->response->addContent('<script src="/vendor/bootstrap_template/vendor/summernote/dist/summernote.start.js"></script>','head');
-    }
+        $this->requireCss('/__asset/osynapsy/Bcl/Summernote/summernote.css');
+        $this->requireJs('/__asset/osynapsy/Bcl/Summernote/summernote.js');
+        $this->requireJs('/__asset/osynapsy/Bcl/Summernote/summernote.start.js');  
+    }    
 }
