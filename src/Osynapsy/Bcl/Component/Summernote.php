e8b1448dc01642d0ab0e0fc1304b52b4c33914e7
Pietro Celeste <p.celeste@osynapsy.org>
2016-12-27 11:01:10 +0100

    Fix assets path for static files

--- src/Osynapsy/Bcl/Component/Summernote.php
+++ src/Osynapsy/Bcl/Component/Summernote.php
@@ -7,8 +7,8 @@ class Summernote extends TextArea
     {
         parent::__construct($name);        
         $this->att('class','summernote');
-        $this->requireCss('/__asset/osynapsy/Bcl/Summernote/summernote.css');
-        $this->requireJs('/__asset/osynapsy/Bcl/Summernote/summernote.js');
-        $this->requireJs('/__asset/osynapsy/Bcl/Summernote/summernote.start.js');  
+        $this->requireCss('/__assets/osynapsy/Bcl/Summernote/summernote.css');
+        $this->requireJs('/__assets/osynapsy/Bcl/Summernote/summernote.js');
+        $this->requireJs('/__assets/osynapsy/Bcl/Summernote/summernote.start.js');  
     }    
 }
