07218cd9d1d79ac124916a09645990a7759afd98
Pietro Celeste <p.celeste@osynapsy.org>
2016-12-27 11:00:55 +0100

    Fix assets path for static files

--- src/Osynapsy/Bcl/Component/SortableList.php
+++ src/Osynapsy/Bcl/Component/SortableList.php
@@ -16,9 +16,9 @@ class SortableList extends ListUnordered
     {
         parent::__construct($name);
         
-        $this->requireCss('/__asset/osynapsy/Bcl/SortableList/style.css');
-        $this->requireJs('/__asset/osynapsy/Bcl/SortableList/jquery.sortable.js');
-        $this->requireJs('/__asset/osynapsy/Bcl/SortableList/script.js');        
+        $this->requireCss('/__assets/osynapsy/Bcl/SortableList/style.css');
+        $this->requireJs('/__assets/osynapsy/Bcl/SortableList/jquery.sortable.js');
+        $this->requireJs('/__assets/osynapsy/Bcl/SortableList/script.js');        
         $this->att('class','sortable-list');
     }
     
