8e233a7d779a75baad80f9dd5a414cbbb820c3a7
Pietro Celeste <p.celeste@osynapsy.org>
2016-12-28 10:09:20 +0100

    Adjust path for lib file

--- src/Osynapsy/Bcl/Component/Multiselect.php
+++ src/Osynapsy/Bcl/Component/Multiselect.php
@@ -19,9 +19,9 @@ class Multiselect extends ComboBox
     public function __construct($name)
     {
         parent::__construct($name);
-        $this->requireJs('/__assets/osynapsy/Bcl/Multiselect/bootstrap-multiselect.js');
-        $this->requireJs('/__assets/osynapsy/Bcl/Multiselect/script.js');
-        $this->requireCss('/__assets/osynapsy/Bcl/Multiselect/style.css');
+        $this->requireCss('/__assets/osynapsy/Lib/boostrap-multiselect-2.0/bootstrap-multiselect.css');
+        $this->requireJs('/__assets/osynapsy/Lib/boostrap-multiselect-2.0/bootstrap-multiselect.js');
+        $this->requireJs('/__assets/osynapsy/Bcl/Multiselect/script.js');        
         $this->setClass('osy-multiselect')->att('multiple','multiple');
         $this->par('option-select-disable',true);
     }
