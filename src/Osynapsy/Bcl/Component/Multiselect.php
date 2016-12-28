93eef8cc3bbf80f6ad125043c291053f14b0a01b
Pietro Celeste <p.celeste@osynapsy.org>
2016-12-28 10:22:47 +0100

    Deleted comments

--- src/Osynapsy/Bcl/Component/Multiselect.php
+++ src/Osynapsy/Bcl/Component/Multiselect.php
@@ -1,18 +1,11 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Osynapsy\Bcl\Component;
 
 use Osynapsy\Ocl\Component\ComboBox;
 /**
  * Description of Multiselect
  *
- * @author Peter
+ * @author p.celeste@osynapsy.org
  */
 class Multiselect extends ComboBox
 {
