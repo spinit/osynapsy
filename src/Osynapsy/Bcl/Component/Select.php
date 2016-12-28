c76b4a84ee473c922905e34baf0f86cff266db60
Pietro Celeste <p.celeste@osynapsy.org>
2016-12-28 10:08:17 +0100

    Adjust path for lib file

--- src/Osynapsy/Bcl/Component/Select.php
+++ src/Osynapsy/Bcl/Component/Select.php
@@ -15,8 +15,8 @@ class Select extends ComboBox2
     {        
         parent::__construct($name);
         $this->class = 'osy-select';
-        $this->requireCss('/__assets/osynapsy/Bcl/Select/bootstrap-select.css');
-        $this->requireJs('/__assets/osynapsy/Bcl/Select/bootstrap-select.js');
+        $this->requireCss('/__assets/osynapsy/Lib/bootstrap-select-1.10.0/bootstrap-select.css');
+        $this->requireJs('/__assets/osynapsy/Lib/bootstrap-select-1.10.0/bootstrap-select.js');
         $this->requireJs('/__assets/osynapsy/Bcl/Select/script.js');
         //$this->par('option-select-disable',false);
         if ($multiple) {
