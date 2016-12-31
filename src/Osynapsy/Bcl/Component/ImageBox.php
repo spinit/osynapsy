f5127ae4bd4a152e6fa66717ceb1dbaca48366d0
Pietro Celeste <p.celeste@osynapsy.org>
2016-12-31 09:37:21 +0100

    Deleted HiddenBox component (for fix bug on refresh method)

--- src/Osynapsy/Bcl/Component/ImageBox.php
+++ src/Osynapsy/Bcl/Component/ImageBox.php
@@ -35,14 +35,14 @@ class ImageBox extends Component
         $this->requireJs('/__assets/osynapsy/Lib/cropper-2.3.2/cropper.js');
         $this->requireCss('/__assets/osynapsy/Bcl/ImageBox/style.css');        
         $this->requireJs('/__assets/osynapsy/Bcl/ImageBox/script.js');
-        parent::__construct('div',$id);
+        parent::__construct('div',$id.'_box');
         $this->att('class','osy-imagebox-bcl')->att('data-action','save');
-        $this->add(new HiddenBox($id));
+        //$this->add(new HiddenBox($id));
         $this->dummy = $this->add(new Tag('label'))
                             ->att('class','osy-imagebox-dummy')
-                            ->att('for',$this->id.'_file');
+                            ->att('for',$id);
         $file = $this->add(new Tag('input'));
-        $file->att('type','file')->att('class','hidden')->att('id',$id.'_file')->name = $id;
+        $file->att('type','file')->att('class','hidden')->att('id',$id)->name = $id;
         
         $this->toolbar = new Tag('div');
         $this->toolbar->att('class','osy-imagebox-bcl-cmd');
@@ -71,10 +71,10 @@ class ImageBox extends Component
     
     private function getImage()
     {
-        if (empty($_REQUEST[$this->id])) {
+        if (empty($_REQUEST[$this->dummy->for])) {
             return;
         }
-        $this->image['webPath'] = $_REQUEST[$this->id];
+        $this->image['webPath'] = $_REQUEST[$this->dummy->for];
         $this->image['diskPath'] = $_SERVER['DOCUMENT_ROOT'].$this->image['webPath'];
         if (file_exists($this->image['diskPath'])) {
             $this->image['dimension'] = getimagesize($this->image['diskPath']);
